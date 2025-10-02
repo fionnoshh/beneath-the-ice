a4a011bb5aa17bb3699197fe0dfb4a971bad7530
fionnoshh <finnoshh@gmail.com>
2025-10-02 14:00:21 -0400

    Update Crew.m

--- Crew.m
+++ Crew.m
@@ -1,1 +1,20 @@
 # Important Crew in the Franklin Expedition
+> A majority of the men who went on this expedition have long since been forgotten. While the show (AMC's *The Terror*, which you should watch if you haven't) helped people know their names, few know more than that. Below you will find a list of the crews of each ship with their names, rank on the ship, age, origin, and any other information known about them.
+___
+### The HMS *Erebus*
+1. Sir John Franklin
+2. James Fitzjames
+3. Graham Gore
+4. Henry Thomas Dundas Le Vesconte
+5. James Walter Fairholme
+6. James Reid
+7. Robert Orme Sargent
+8. Charles Frederick Des Voeux
+9. Edward Couch
+10. Henry Foster Collins
+11. Edward Couch
+12. Henry Foster Collins
+13. Stephen Samuel Stanley
+14. Harry Duncan Spens Goodsir
+15. Charles Hamilton Osmer
+16. Thomas Terry
